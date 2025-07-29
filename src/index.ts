5bfbb06db8f85add3fb72f8226db053ef01768c7
Vitor Froes <vitor.souza@hiplatform.com>
2025-07-29 11:30:59 -0300

    feat(main): configure swagger, swagger-ui and autoload

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import Fastify from "fastify";
+import path from "path";
 import { OTPService } from "./domain/services/otpService";
 import { otpRoutes } from "./infrastructure/web/otpController";
 import { InMemoryOTPRepository } from "./adapters/repository/inMemoryOtpRepository";
@@ -6,6 +7,42 @@ import { GenerateOTP } from "./application/useCases/generateOtp";
 
 async function start() {
   const fastify = Fastify({ logger: true });
+
+  await fastify.register(require("@fastify/swagger"), {
+    swagger: {
+      info: {
+        title: "OTP API",
+        description: "API to manage OTP tokens",
+        version: "1.0.0",
+      },
+    },
+  });
+
+  fastify.register(require("@fastify/autoload"), {
+    dir: path.join(__dirname, "infrastructure", "web"),
+  });
+
+  await fastify.register(import("@fastify/swagger-ui"), {
+    routePrefix: "/documentation",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) {
+        next();
+      },
+      preHandler: function (request, reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject, request, reply) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
 
   fastify.get("/", async (request, reply) => {
     return { hello: "world" };
@@ -19,6 +56,8 @@ async function start() {
 
   await fastify.listen({ port: 3000 });
   fastify.log.info(`Server listening on http://localhost:3000`);
+
+  await fastify.ready();
 }
 
 start().catch(console.error);
